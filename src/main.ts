b8bb9edbda3de9ecf30fe6b5470801016a2ebcb3
JSarmientoP <jsarmientos@redprogrentis.com>
2022-10-07 17:24:46 +0100

    WIP: dist

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import "./assets/style/main.css";
-import Vue3Katex from '@hsorby/vue3-katex';
-import 'katex/dist/katex.min.css';
+import './assets/style/main.css'
+import Vue3Katex from '@hsorby/vue3-katex'
+import 'katex/dist/katex.min.css'
 
-createApp(App).use(Vue3Katex).mount("#app");
+createApp(App).use(Vue3Katex).mount('#app')
